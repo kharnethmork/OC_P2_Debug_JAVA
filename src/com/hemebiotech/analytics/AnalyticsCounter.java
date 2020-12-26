654ce918fcccfa2f48597d27f10f32a83f9d006a
Daïna Roque <dainaroque@gmail.com>
2020-12-26 11:31:41 +0100

    Projet 2

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,34 +3,89 @@ package com.hemebiotech.analytics;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AnalyticsCounter {
-  
-/**
- *
- *The filepath of the files.
- */
+
+    /**
+     * The filepath of the files.
+     */
     public static final String SYMPTOMS_TXT = "src/ressources/symptoms.txt";
     public static final String RESULTATS_TXT = "src/results/resultats.out";
 
     /**
-     *
-     *To start the program.
+     * Read and sort the symptom's list
+     */
+    public static class SymptomsFileReaderAndWriter {
+
+        public List<String> readSymptomsFromFile(String filepath) {
+
+            ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile(filepath);
+            List<String> symptoms = readSymptomDataFromFile.GetSymptoms();
+            Collections.sort(symptoms);
+            return symptoms;
+        }
+
+        /**
+         * Delete the results file if exists and write a new one.
+         *
+         * @param mapOfSymptoms
+         * @param filepath
+         * @throws IOException
+         */
+        public void writeSymptomsInResultsFile(Map<String, Integer> mapOfSymptoms, String filepath) throws IOException {
+
+            File resultsFile = new File(filepath);
+            resultsFile.exists();
+            if (resultsFile.exists()) {
+                resultsFile.delete();
+            }
+
+            FileWriter writer = new FileWriter(filepath);
+            for (Map.Entry<String, Integer> entry : mapOfSymptoms.entrySet()) {
+                writer.write(entry.getKey() + " : " + entry.getValue() + " \n ");
+            }
+
+            writer.close();
+        }
+    }
+
+    public static class AnalyseSymptoms {
+
+        /**
+         * To analyse and count the symptoms.
+         */
+        public Map<String, Integer> getMapOfSymptoms(List<String> listOfSymptoms) {
+
+            Collections.sort(listOfSymptoms);
+
+            Map<String, Integer> mapOfSymptoms = new LinkedHashMap<>();
+
+            for (String symptom : listOfSymptoms) {
+                if (!mapOfSymptoms.containsKey(symptom)) {
+                    mapOfSymptoms.put(symptom, 0);
+                }
+            }
+            for (String symptom : listOfSymptoms) {
+                mapOfSymptoms.replace(symptom, mapOfSymptoms.get(symptom) + 1);
+            }
+            return mapOfSymptoms;
+        }
+    }
+
+    /**
+     * To start the program.
      */
     public static void main(String args[]) throws Exception {
 
        SymptomsFileReaderAndWriter symptomsFileReaderAndWriter = new SymptomsFileReaderAndWriter();
        AnalyseSymptoms analyseSymptoms = new AnalyseSymptoms();
 
-       List<String> listOfSymptoms = symptomsFileReaderAndWriter.readSymptomsFromFile(SYMPTOMS_TXT);
+        List<String> listOfSymptoms = symptomsFileReaderAndWriter.readSymptomsFromFile(SYMPTOMS_TXT);
 
-       Map<String, Integer> mapOfSymptoms = analyseSymptoms.getMapOfSymptoms(listOfSymptoms);
+        Map<String, Integer> mapOfSymptoms = analyseSymptoms.getMapOfSymptoms(listOfSymptoms);
 
-       symptomsFileReaderAndWriter.writeSymptomsInResultsFile(mapOfSymptoms, RESULTATS_TXT);
+        symptomsFileReaderAndWriter.writeSymptomsInResultsFile(mapOfSymptoms, RESULTATS_TXT);
     }
 
 }
